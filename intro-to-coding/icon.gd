cf70bd5d120ddbc39e22fa442a71787f5ae3b053
bryanjitsen <bryanjitsen@gmail.com>
2026-02-16 18:14:07 -0500

    added lab 2

---
+++ intro-to-coding/icon.gd
@@ -0,0 +1,35 @@
+extends Sprite2D
+
+@export var speed = 5
+
+@export var my_name = ""
+
+@export var moving = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	print("Hello World")
+	print('my name is', my_name)
+	print('am i moving is', moving)
+
+
+#func _input(event: InputEvent) -> void:
+	#print(event)
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	#if (Input.is_action_just_pressed("ui_accept")):
+		position.x += speed
+	#else:
+		position.y += speed
+	if Input.is_action_pressed("ui_up"):
+		position.y += speed
+	if Input.is_action_pressed("ui_down"):
+		position.y -= speed
+		if Input.is_action_pressed("ui_right"):
+		position.x += speed
+	if Input.is_action_pressed("ui_left"):
+		position.x -= speed
+	
