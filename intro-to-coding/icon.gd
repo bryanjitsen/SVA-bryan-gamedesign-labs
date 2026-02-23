7073ec408a2053e5ce9775d0feeec84180d875cb
bryanjitsen <bryanjitsen@gmail.com>
2026-02-23 10:24:14 -0500

    fixed code

--- intro-to-coding/icon.gd
+++ intro-to-coding/icon.gd
@@ -28,7 +28,7 @@ func _process(delta: float) -> void:
 		position.y += speed
 	if Input.is_action_pressed("ui_down"):
 		position.y -= speed
-		if Input.is_action_pressed("ui_right"):
+	if Input.is_action_pressed("ui_right"):
 		position.x += speed
 	if Input.is_action_pressed("ui_left"):
 		position.x -= speed
